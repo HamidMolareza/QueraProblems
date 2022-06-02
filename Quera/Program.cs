cbb4fe47fcca35f8ca8f357bd4eeeb413e95347b
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-02 15:44:29 +0430

    Resolved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Quera {
     public static class Program {
         public static void Main() {
-            var input = Console.ReadLine();
-            var n = Convert.ToInt32(input);
-
-            var result = 1;
-            for (var i= n; i > 1; i--) {
-                result *= i;
-            }
-
-            Console.WriteLine(result);
+            Console.ReadLine()
+                .Trim()
+                .Split(' ')
+                .Select(number => Convert.ToInt32(number))
+                .ToList()
+                .IsTriangle()
+                .Print();
         }
+
+        private static bool IsTriangle(this IReadOnlyCollection<int> numbers) =>
+            numbers.Sum() == 180 && numbers.All(num => num > 0);
+
+        private static void Print(this bool @this) => Console.WriteLine(@this ? "Yes" : "No");
     }
 }
