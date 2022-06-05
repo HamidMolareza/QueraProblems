b164f07f886acf6232c97c787672051ccddcfb33
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-05 12:47:46 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Quera {
     public static class Program {
+        private const int NumOfUsers = 3;
+        private const int NumOfWeekDays = 7;
+        
         public static void Main() {
+            var daysCount = GetDaysCount();
+            Console.WriteLine(NumOfWeekDays - daysCount);
         }
 
-        private static List<int> GetNumbers(int count) {
-            var result = new List<int>(count);
-            for (var i = 0; i < count; i++) {
-                result.Add(Convert.ToInt32(Console.ReadLine()));
+        private static int GetDaysCount() {
+            var uniqueDays = new HashSet<string>();
+            for (var i = 0; i < NumOfUsers; i++) {
+                Console.ReadLine(); //Skip
+                var days = Console.ReadLine()
+                    .Split(' ');
+                uniqueDays.AddRange(days);
             }
 
-            return result;
+            return uniqueDays.Count;
+        }
+
+        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
+            foreach (var item in list) {
+                hashList.Add(item);
+            }
         }
     }
 }
