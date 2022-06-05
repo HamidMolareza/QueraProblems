5385f07573ded018219851261011f91aa8155b57
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-05 12:49:58 +0430

    Adds AddRange

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -3,26 +3,18 @@ using System.Collections.Generic;
 
 namespace Quera {
     public static class Program {
-        private const int NumOfUsers = 3;
-        private const int NumOfWeekDays = 7;
-        
         public static void Main() {
-            var daysCount = GetDaysCount();
-            Console.WriteLine(NumOfWeekDays - daysCount);
         }
 
-        private static int GetDaysCount() {
-            var uniqueDays = new HashSet<string>();
-            for (var i = 0; i < NumOfUsers; i++) {
-                Console.ReadLine(); //Skip
-                var days = Console.ReadLine()
-                    .Split(' ');
-                uniqueDays.AddRange(days);
+        private static List<int> GetNumbers(int count) {
+            var result = new List<int>(count);
+            for (var i = 0; i < count; i++) {
+                result.Add(Convert.ToInt32(Console.ReadLine()));
             }
 
-            return uniqueDays.Count;
+            return result;
         }
-
+        
         private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
             foreach (var item in list) {
                 hashList.Add(item);
