d368e3ed5fe58bfd5be0fdeabd8d10fadb829762
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-10 21:13:00 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 
 namespace Quera {
     public static class Program {
-        public static void Main() {
+        public static void Main() =>
+            GetInputs<string>(2)
+                .Compair();
+
+        private static void Compair(this IReadOnlyList<string> numbers) {
+            for (var i = 2; i >= 0; i--) {
+                if (numbers[0][i] < numbers[1][i]) {
+                    Console.WriteLine($"{numbers[0]} < {numbers[1]}");
+                    return;
+                }
+                if (numbers[0][i] > numbers[1][i]) {
+                    Console.WriteLine($"{numbers[1]} < {numbers[0]}");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{numbers[0]} = {numbers[1]}");
         }
 
-        private static List<int> GetNumbers(int count) {
-            var result = new List<int>(count);
+        private static List<T> GetInputs<T>(int count) {
+            var result = new List<T>(count);
             for (var i = 0; i < count; i++) {
-                result.Add(Convert.ToInt32(Console.ReadLine()));
+                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
             }
 
             return result;
-        }
-        
-        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
-            foreach (var item in list) {
-                hashList.Add(item);
-            }
         }
     }
 }
