61ab06cc4bdbfd0f8ccaccf184c9c5bea2ad44fa
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-18 16:00:06 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var input = GetInputs<string>(1).Single();
+            Console.WriteLine(input);
+
+            for (var i = 1; i < input.Length; i++) {
+                var targetChar = Convert.ToChar(input.Substring(i, 1));
+                var text = new string(targetChar, i); //Repeat char i times.
+                text += input.Substring(i); //Adds remain chars
+                Console.WriteLine(text);
+            }
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -13,12 +23,6 @@ namespace Quera {
             }
 
             return result;
-        }
-        
-        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
-            foreach (var item in list) {
-                hashList.Add(item);
-            }
         }
     }
 }
