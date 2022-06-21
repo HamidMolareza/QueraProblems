f59673ee21a2b51b06db64e213fbbbfe980991e2
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-21 20:15:04 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,6 +5,23 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            GetInputs<int>(1); //Ignore
+            var watermelons = GetInputs<int>(' ')
+                .Select((watermelon, index) => new KeyValuePair<int, int>(index, watermelon))
+                .ToList();
+
+            var result = Eat(watermelons);
+
+            Console.WriteLine(result.Key + 1);
+        }
+
+        private static KeyValuePair<int, int> Eat(IList<KeyValuePair<int, int>> watermelons) {
+            while (true) {
+                if (watermelons.Count == 1)
+                    return watermelons.First();
+
+                watermelons.RemoveAt(watermelons[0].Value > watermelons[1].Value ? 1 : 0);
+            }
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -15,16 +32,11 @@ namespace Quera {
 
             return result;
         }
-        
+
         private static List<T> GetInputs<T>(char separator) =>
             Console.ReadLine()
-                ?.Split(separator)
+                ?.Trim()
+                .Split(separator)
                 .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
-        
-        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
-            foreach (var item in list) {
-                hashList.Add(item);
-            }
-        }
     }
 }
