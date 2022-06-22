2f38ca457935e9e2991bcf62869b65cc3f26276c
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-22 13:59:16 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,27 +5,41 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var input = GetInput(3);
+
+            var uniqueX = input.XValues
+                .GetUniqueValue();
+            var uniqueY = input.YValues
+                .GetUniqueValue();
+
+            Console.WriteLine($"{uniqueX} {uniqueY}");
         }
 
-        private static List<T> GetInputs<T>(int count) {
-            var result = new List<T>(count);
+        private static T GetUniqueValue<T>(this IEnumerable<T> items) =>
+            items.GroupBy(t => t)
+                .Single(grouping => grouping.Count() == 1).Key;
+
+        private static Input GetInput(int count) {
+            var result = new Input();
+
             for (var i = 0; i < count; i++) {
-                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
+                var input = GetInputs<int>(' ');
+                result.XValues.Add(input[0]);
+                result.YValues.Add(input[1]);
             }
 
             return result;
         }
-        
+
         private static List<T> GetInputs<T>(char separator) =>
             Console.ReadLine()
                 ?.Trim()
                 .Split(separator)
                 .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
-        
-        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
-            foreach (var item in list) {
-                hashList.Add(item);
-            }
-        }
+    }
+    
+    public class Input {
+        public List<int> XValues { get; set; } = new List<int>();
+        public List<int> YValues { get; set; } = new List<int>();
     }
 }
