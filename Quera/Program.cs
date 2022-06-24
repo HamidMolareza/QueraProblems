ea500c7eefe52d053ced2333778685eb37fc2cb9
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-24 05:17:42 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,27 +5,29 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
-        }
+            var inputs = GetInputs<int>(' ');
+            var numOfPersons = inputs[0];
+            var k = inputs[1];
 
-        private static List<T> GetInputs<T>(int count) {
-            var result = new List<T>(count);
-            for (var i = 0; i < count; i++) {
-                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
+            var previousPerson = 1;
+            var i = 1;
+            for (;; i++) {
+                var currentPerson = previousPerson + k;
+                if (currentPerson > numOfPersons)
+                    currentPerson %= numOfPersons;
+                
+                if (currentPerson == 1)
+                    break;
+                previousPerson = currentPerson;
             }
 
-            return result;
+            Console.WriteLine(i);
         }
-        
+
         private static List<T> GetInputs<T>(char separator) =>
             Console.ReadLine()
                 ?.Trim()
                 .Split(separator)
                 .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
-        
-        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
-            foreach (var item in list) {
-                hashList.Add(item);
-            }
-        }
     }
 }
