f8e6628d502ef69846fe892a53336f2f9755ca20
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-24 05:36:29 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -4,7 +4,21 @@ using System.Linq;
 
 namespace Quera {
     public static class Program {
+        private const int NumOfInputsPerCities = 2;
+
         public static void Main() {
+            var cities = GetInputs(3);
+            var result = cities.Select(city => city.Min()).Sum();
+            Console.WriteLine(result);
+        }
+
+        private static List<List<int>> GetInputs(int numOfCities) {
+            var result = new List<List<int>>(numOfCities);
+            for (var i = 0; i < numOfCities; i++) {
+                result.Add(GetInputs<int>(NumOfInputsPerCities));
+            }
+
+            return result;
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -14,18 +28,6 @@ namespace Quera {
             }
 
             return result;
-        }
-        
-        private static List<T> GetInputs<T>(char separator) =>
-            Console.ReadLine()
-                ?.Trim()
-                .Split(separator)
-                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
-        
-        private static void AddRange<T>(this ISet<T> hashList, IEnumerable<T> list) {
-            foreach (var item in list) {
-                hashList.Add(item);
-            }
         }
     }
 }
