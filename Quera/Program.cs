056cfbdf4351541794ba97e75dd33f2a661e2d1e
Hamid Molareza <kitm.hamid@gmail.com>
2022-06-29 10:49:05 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,6 +5,16 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var inputs = GetInputs<int>(' ');
+
+            var diff = inputs[1] - inputs[0];
+            if (diff == 0) {
+                Console.WriteLine("Saal Noo Mobarak!");
+            }
+            else {
+                var direction = diff > 0 ? 'R' : 'L';
+                Console.WriteLine(new string(direction, Math.Abs(diff)));
+            }
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -15,7 +25,7 @@ namespace Quera {
 
             return result;
         }
-        
+
         private static List<T> GetInputs<T>(char separator) =>
             Console.ReadLine()
                 ?.Trim()
