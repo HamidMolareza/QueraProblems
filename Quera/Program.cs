0f1f44fe94ea691bad6804328136855a7f37fe69
Hamid Molareza <kitm.hamid@gmail.com>
2022-07-01 13:56:20 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,21 +5,33 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var input = GetInputs();
+
+            var sumOfJams = input.Jams.Sum() * 1.0;
+            var result = input.NumOfJars - Math.Ceiling(sumOfJams / input.Capacity);
+
+            Console.WriteLine(result);
         }
 
-        private static List<T> GetInputs<T>(int count) {
-            var result = new List<T>(count);
-            for (var i = 0; i < count; i++) {
-                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
-            }
-
-            return result;
+        private static Input GetInputs() {
+            var inputs = GetInputs<int>(' ');
+            return new Input {
+                NumOfJars = inputs[0],
+                Capacity = inputs[1],
+                Jams = GetInputs<int>(' ')
+            };
         }
-        
+
         private static List<T> GetInputs<T>(char separator) =>
             Console.ReadLine()
                 ?.Trim()
                 .Split(separator)
                 .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
+    }
+
+    public class Input {
+        public int NumOfJars { get; set; }
+        public int Capacity { get; set; }
+        public List<int> Jams { get; set; }
     }
 }
