e362063bf1e5c115d5b5fcef0233914dcb106c30
Hamid Molareza <kitm.hamid@gmail.com>
2022-07-08 13:45:40 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var inputs = GetInputs<decimal>(2);
+            var biggerNumber = inputs[0] > inputs[1] ? inputs[0] : inputs[1];
+            var smallerNumber = inputs[0] > inputs[1] ? inputs[1] : inputs[0];
+
+            var gcd = GCD(biggerNumber, smallerNumber);
+
+            Console.WriteLine(gcd);
+        }
+
+        private static decimal GCD(decimal biggerNumber, decimal smallerNumber) {
+            var remain = biggerNumber % smallerNumber;
+            return remain == 0
+                ? smallerNumber
+                : GCD(smallerNumber, remain);
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -15,11 +28,5 @@ namespace Quera {
 
             return result;
         }
-        
-        private static List<T> GetInputs<T>(char separator) =>
-            Console.ReadLine()
-                ?.Trim()
-                .Split(separator)
-                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
     }
 }
