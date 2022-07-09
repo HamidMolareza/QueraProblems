c6568884f34281ec4899899fcc16f860a49268e6
Hamid Molareza <kitm.hamid@gmail.com>
2022-07-09 11:26:04 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,17 +5,12 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var result = GetInputs<double>(' ')
+                .Aggregate((multiplication, number) => multiplication * number);
+
+            Console.WriteLine(result);
         }
 
-        private static List<T> GetInputs<T>(int count) {
-            var result = new List<T>(count);
-            for (var i = 0; i < count; i++) {
-                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
-            }
-
-            return result;
-        }
-        
         private static List<T> GetInputs<T>(char separator) =>
             Console.ReadLine()
                 ?.Trim()
