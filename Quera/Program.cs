781bc10830feaa845fe1fe32518d9c0a90c554ee
Hamid Molareza <kitm.hamid@gmail.com>
2022-07-19 11:43:34 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,6 +5,33 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var inputs = GetInputs<int>(2);
+
+            PrintSquare(inputs[0], inputs[1]);
+        }
+
+        private static void PrintSquare(int outerSquare, int innerSquare) {
+            if (innerSquare >= outerSquare) {
+                Console.WriteLine("Wrong order!");
+                return;
+            }
+
+            var diff = outerSquare - innerSquare;
+            if (diff % 2 != 0) {
+                Console.WriteLine("Wrong difference!");
+                return;
+            }
+
+            var line = string.Concat(Enumerable.Repeat("* ", outerSquare));
+            var innerLineStars = string.Concat(Enumerable.Repeat("* ", diff / 2));
+            var innerLine = innerLineStars + string.Concat(Enumerable.Repeat("  ", innerSquare)) + innerLineStars;
+
+            for (var i = 0; i < diff / 2; i++)
+                Console.WriteLine(line);
+            for (var i = 0; i < innerSquare; i++)
+                Console.WriteLine(innerLine);
+            for (var i = 0; i < diff / 2; i++)
+                Console.WriteLine(line);
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -15,11 +42,5 @@ namespace Quera {
 
             return result;
         }
-        
-        private static List<T> GetInputs<T>(char separator) =>
-            Console.ReadLine()
-                ?.Trim()
-                .Split(separator)
-                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
     }
 }
