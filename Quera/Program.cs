ec1897dddd198d7cd3a62ce05baf7fd485a8384d
Hamid Molareza <kitm.hamid@gmail.com>
2022-07-21 03:22:10 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -4,7 +4,44 @@ using System.Linq;
 
 namespace Quera {
     public static class Program {
+        private static readonly Person Keyvoon = new Person("keyvoon", "331122");
+        private static readonly Person Nezam = new Person("nezam", "123");
+        private static readonly Person Shirfarhad = new Person("shir farhad", "2123");
+
         public static void Main() {
+            //Input
+            var _ = Console.ReadLine(); //Ignore n
+            var keys = GetInputs<string>(1).Single();
+
+            //Process
+            var persons = Evaluate(keys, Keyvoon, Nezam, Shirfarhad)
+                .OrderByDescending(result => result.Score)
+                .ThenBy(result => result.Name)
+                .ToList();
+
+            //Output
+            var maximumScore = persons.First().Score;
+            Console.WriteLine(maximumScore);
+
+            foreach (var person in persons.Where(person => person.Score == maximumScore)) {
+                Console.WriteLine(person.Name);
+            }
+        }
+
+        private static IEnumerable<Person> Evaluate(string keys, params Person[] persons) {
+            var result = persons.ToList(); //Get copy of input
+
+            for (var i = 0; i < keys.Length; i++) {
+                foreach (var person in result) {
+                    var personIndexAnswer = i % person.Pattern.Length;
+                    var personAnswer = person.Pattern[personIndexAnswer];
+                    if (personAnswer == keys[i]) {
+                        person.Score++;
+                    }
+                }
+            }
+
+            return result;
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -15,11 +52,16 @@ namespace Quera {
 
             return result;
         }
-        
-        private static List<T> GetInputs<T>(char separator) =>
-            Console.ReadLine()
-                ?.Trim()
-                .Split(separator)
-                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
+    }
+
+    public class Person {
+        public Person(string name, string pattern) {
+            Name = name;
+            Pattern = pattern;
+        }
+
+        public string Name { get; set; }
+        public string Pattern { get; set; }
+        public int Score { get; set; } = 0;
     }
 }
