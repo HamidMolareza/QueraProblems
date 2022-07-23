171de92db9e7206d80028d5e3bb70b98272dda3c
Hamid Molareza <kitm.hamid@gmail.com>
2022-07-23 10:40:39 +0430

    Solved

--- Quera/Program.cs
+++ Quera/Program.cs
@@ -5,12 +5,31 @@ using System.Linq;
 namespace Quera {
     public static class Program {
         public static void Main() {
+            var input = GetInput();
+
+            var currentState = input.FirstState;
+            foreach (var move in input.Moves) {
+                if (move.From == currentState) {
+                    currentState = move.To;
+                }
+                else if(move.To == currentState) {
+                    currentState = move.From;
+                }
+            }
+
+            Console.WriteLine(currentState);
         }
 
-        private static List<T> GetInputs<T>(int count) {
-            var result = new List<T>(count);
-            for (var i = 0; i < count; i++) {
-                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
+        private static Input GetInput() {
+            var result = new Input();
+            
+            var line = GetInputs<string>(' ');
+            result.FirstState = line[1];
+            var numOfMoves = Convert.ToInt32(line[0]);
+            
+            for (var i = 0; i < numOfMoves; i++) {
+               line = GetInputs<string>(' ');
+               result.Moves.Add(new Movement(line[0], line[1]));
             }
 
             return result;
@@ -21,5 +40,20 @@ namespace Quera {
                 ?.Trim()
                 .Split(separator)
                 .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
+    }
+
+    public class Input {
+        public string FirstState { get; set; }
+        public List<Movement> Moves { get; set; } = new();
+    }
+
+    public class Movement {
+        public Movement(string @from, string to) {
+            From = @from;
+            To = to;
+        }
+        
+        public string From { get; set; }
+        public string To { get; set; }
     }
 }
