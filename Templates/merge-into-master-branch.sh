284e7fb69262b59949795d79166568b1b39e7040
Hamid Molareza <kitm.hamid@gmail.com>
2022-08-27 03:35:55 +0430

    Updates merge-into-master-branch.sh

--- Templates/merge-into-master-branch.sh
+++ Templates/merge-into-master-branch.sh
@@ -21,23 +21,30 @@ if [ -z "$queraId" ]; then
   printf "Quera Id: "
   read -r queraId
 fi
+
+deleteBranch="$2"
 #===========================================================
 
-#checkout to master
+#checkout to master branch
 git checkout "master" --quiet
 exit_if_operation_failed "$?" "Checkout to master branch failed."
 wait
 
+#merge into master branch
 git merge "$queraId" --quiet
 exit_if_operation_failed "$?" "Merge $queraId to master branch failed."
 wait
 
-printf "Do you want delete branch $queraId (y/N): "
-read -r deleteBranch
-if [ "$deleteBranch" -eq 'y' ] && [ "$deleteBranch" -eq 'Y' ]; then
-  git branch --delete "$queraId" --quiet
+echo "The operation was completed successfully."
+
+#delete branch
+if [ -z "$deleteBranch" ]; then
+  printf "Do you want delete branch $queraId (y/N): "
+  read -r deleteBranch
+fi
+if [ "$deleteBranch" == 'y' ] || [ "$deleteBranch" == 'Y' ]; then
+  git branch -D "$queraId" --quiet
   warning_if_operation_failed "$?" "delete branch $queraId failed."
 fi
 
-echo "The operation was completed successfully."
 echo "Please don't forget to push commits."
