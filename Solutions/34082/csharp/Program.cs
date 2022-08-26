43683d124980bf5a42ee170960118f8849b3780c
Hamid Molareza <kitm.hamid@gmail.com>
2022-08-27 02:40:49 +0430

    Solved 34082

---
+++ Solutions/34082/csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            var capacity = GetInputs<int>(' ').Skip(1).Single();
+            var numOfGroupMembers = GetInputs<int>(' ');
+
+            var minimumShots = 0;
+            var sum = 0;
+            foreach (var numOfGroupMember in numOfGroupMembers) {
+                sum += numOfGroupMember;
+                if (sum <= capacity)
+                    continue;
+                sum = numOfGroupMember;
+                minimumShots++;
+            }
+
+            if (sum > 0)
+                minimumShots++;
+
+            Console.WriteLine(minimumShots);
+        }
+
+        private static List<T> GetInputs<T>(char separator) =>
+            Console.ReadLine()
+                ?.Trim()
+                .Split(separator)
+                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
+    }
+}
