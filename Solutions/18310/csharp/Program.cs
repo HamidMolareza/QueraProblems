8cb76577f408aa5ac321e90caad1891ee5302d46
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-05 18:22:26 +0430

    Solved 18310

---
+++ Solutions/18310/csharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            GetInputs()
+                .CanWinPrize()
+                .PrintLine(canWin => canWin ? "Yes" : "No");
+        }
+
+        private static Input GetInputs() => new Input {
+            CircleLetters = GetInputs<string>(1).Single(),
+            TargetString = GetInputs<string>(1).Single()
+        };
+
+        private static bool CanWinPrize(this Input input) =>
+            FindStartIndexes(input)
+                .Any(startIndex => IsMatch(input, startIndex));
+
+        private static IEnumerable<int> FindStartIndexes(Input input) {
+            var indexes = new List<int>();
+            for (var i = 0; i < input.CircleLetters.Length; i++) {
+                if (input.CircleLetters[i] == input.TargetString[0])
+                    indexes.Add(i);
+            }
+
+            return indexes;
+        }
+
+        private static bool IsMatch(Input input, int startIndex) {
+            for (var targetIndex = 0; targetIndex < input.TargetString.Length; targetIndex++) {
+                var circleIndex = (targetIndex + startIndex) % input.CircleLetters.Length;
+                if (input.CircleLetters[circleIndex] != input.TargetString[targetIndex])
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static List<T> GetInputs<T>(int count) {
+            var result = new List<T>(count);
+            for (var i = 0; i < count; i++) {
+                result.Add((T) Convert.ChangeType(Console.ReadLine(), typeof(T)));
+            }
+
+            return result;
+        }
+
+        private static void PrintLine<TSource, TResult>(
+            this TSource source,
+            Func<TSource, TResult> func
+        ) => Console.WriteLine(func(source));
+    }
+
+    public class Input {
+        public string CircleLetters { get; set; }
+        public string TargetString { get; set; }
+    }
+}
---
+++ Solutions/18310/csharp/README.md
@@ -0,0 +1,28 @@
+# About this solution
+
+This solution has scored 100 in [Quera](https://quera.org/).
+
+🌟 If you like this solution, please give it a star.
+
+## Built With
+
+C# .NET 5
+
+# Usage
+
+1. In the directory, run `dotnet build`
+2. `dotnet run`
+
+## Support
+
+Reach out to the maintainer at one of the following places:
+
+- [GitHub issues](https://github.com/HamidMolareza/QueraProblems/issues/new?assignees=&labels=question&template=04_SUPPORT_QUESTION.md&title=support%3A+)
+
+## Authors & contributors
+
+The original setup of this repository is by [Hamid Molareza](https://github.com/HamidMolareza).
+
+## License
+
+This solution is licensed under the [GPLv3](https://choosealicense.com/licenses/gpl-3.0/).
