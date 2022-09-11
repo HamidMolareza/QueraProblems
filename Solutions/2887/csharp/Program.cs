b0126d343267fec07ee5638ab0061907d1ee29a2
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-11 19:30:31 +0430

    Solved 2887

---
+++ Solutions/2887/csharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            Console.ReadLine(); //Skip line
+            var claims = GetInputs<int>(' ');
+
+            var gcd = claims.GetGcd();
+            var numOfObjects = claims.Sum(claim => claim / gcd);
+
+            Console.WriteLine(numOfObjects);
+        }
+
+        private static long GetGcd(this IReadOnlyList<int> numbers) {
+            long gcd = numbers[0];
+            for (var i = 1; i < numbers.Count; i++)
+                gcd = GetGcd(gcd, numbers[i]);
+            return gcd;
+        }
+
+        private static long GetGcd(long num1, long num2) {
+            var biggerNumber = num1 >= num2 ? num1 : num2;
+            var smallerNumber = num1 >= num2 ? num2 : num1;
+
+            while (smallerNumber > 0) {
+                var remain = biggerNumber % smallerNumber;
+                biggerNumber = smallerNumber;
+                smallerNumber = remain;
+            }
+
+            return biggerNumber;
+        }
+
+        private static List<T> GetInputs<T>(char separator) =>
+            Console.ReadLine()
+                ?.Trim()
+                .Split(separator)
+                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList()
+            ?? new List<T>();
+    }
+}
