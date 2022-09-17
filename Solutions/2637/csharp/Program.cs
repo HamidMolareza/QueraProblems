6f6697c871b1a4a0778fffee98f85cb91b56c545
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-18 01:33:08 +0430

    Reformat 2637 base Quera structure

--- Solutions/2637/csharp/Program.cs
+++ Solutions/2637/csharp/Program.cs
@@ -7,8 +7,8 @@ namespace Quera {
         public static void Main() {
             var n = GetInputs<int>(1).Single();
 
-            Console.WriteLine($"Solution1: {Solution1.GetMaximumSquares(n)}");
-            Console.WriteLine($"Solution2: {Solution2.GetMaximumSquares(n)}");
+            Console.WriteLine(Solution1.GetMaximumSquares(n));
+            Console.WriteLine(Solution2.GetMaximumSquares(n));
         }
 
         private static List<T> GetInputs<T>(int count) {
@@ -19,5 +19,47 @@ namespace Quera {
 
             return result;
         }
+    }
+
+    public static class Solution1 {
+        public static int GetMaximumSquares(int n) =>
+            2 + SumOfSequence(n - 1); // 2 + 2,2,3,3,4,4,5,5,...
+
+        private static int SumOfSequence(int n) {
+            if (n < 1) return 0;
+            var sequenceValue = GetSequenceValue(n); //Sequence: 2,2,3,3,4,4,5,5,...
+
+            /*
+         * If n = 4
+         * 2 + 2 + 3 + 3 = 2 * (2 + 3) = 2 * (1 + 2 + 3 - 1)
+         * 1 + 2 + 3 + ... = n(n+1)/2
+         */
+            var result = 2 * (sequenceValue * (sequenceValue + 1) / 2 - 1);
+
+            /*
+         * If n = 2 --> 2 + 2 = 2(2)
+         * But if n is odd like n = 3 ---> 2 + 2 + 3 = (2 * 5) - 3
+         */
+            if (n.IsOdd())
+                result -= sequenceValue;
+
+            return result;
+        }
+
+        private static int GetSequenceValue(int n) {
+            //Sequence: 2,2,3,3,4,4,5,5,...
+            if (n < 1) return 0;
+            if (!n.IsOdd())
+                n -= 1;
+            return (int) (Math.Floor(n / 2.0) + 2);
+        }
+
+        private static bool IsOdd(this int number) =>
+            number % 2 != 0;
+    }
+
+    public static class Solution2 {
+        public static int GetMaximumSquares(int n) =>
+            (n / 2 + 1) * ((n + 1) / 2 + 1);
     }
 }
--- Solutions/2637/csharp/Solution2.cs
+++
@@ -1,6 +0,0 @@
-namespace Quera; 
-
-public static class Solution2 {
-    public static int GetMaximumSquares(int n) =>
-        (n / 2 + 1) * ((n + 1) / 2 + 1);
-}
