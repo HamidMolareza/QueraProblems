f91fa973dfdd74a869d0643e65f623c7168d6c51
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-18 00:23:05 +0430

    Adds utility function to py template

---
+++ Templates/python3/program.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def get_inputs(num_of_lines: int) -> List[str]:
+    result = []
+    for i in range(num_of_lines):
+        result.append(input())
+    return result
+
+
+# Sample
+num_of_inputs = 4
+inputs = list(map(float, get_inputs(num_of_inputs)))
