6ffee5ae87b730baf50ca0f7f0369bcd7c471ef4
Hamid Molareza <kitm.hamid@gmail.com>
2022-09-17 23:44:14 +0430

    Solves 3403 with python

---
+++ Solutions/3403/python3/program.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def get_inputs(num_of_lines: int) -> List[str]:
+    result = []
+    for i in range(num_of_lines):
+        result.append(input())
+    return result
+
+
+def multiply(input_list: List[float]) -> float:
+    result = 1
+    for number in input_list:
+        result *= number
+    return result
+
+
+num_of_inputs = 4
+inputs = list(map(float, get_inputs(num_of_inputs)))
+
+summation = sum(inputs)
+average = summation / num_of_inputs
+product = multiply(inputs)
+maximum = max(inputs)
+minimum = min(inputs)
+
+print('Sum : %.6f' % summation)
+print('Average : %.6f' % average)
+print('Product : %.6f' % product)
+print('Max : %.6f' % maximum)
+print('Min : %.6f' % minimum)
