afaff8537bea96fb19b83c41c7cd52d1c9ad899c
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-14 20:35:56 +0330

    Fixes bug

--- Solutions/10233/csharp/Solution2.cs
+++ Solutions/10233/csharp/Solution2.cs
@@ -4,16 +4,14 @@ namespace Quera {
     public static class Solution2 {
         public static string Solve(string number) {
             for (var i = number.Length - 2; i >= 0; i--) {
-                var remainDigits = number.Remove(0, number.Length - i)
-                    .Append(number[i])
+                var remainDigits = number.Remove(0, i)
                     .OrderBy(digit => digit).ToList();
-                var minimumDigit = remainDigits.SingleOrDefault(digit => digit > number[i]);
+                var minimumDigit = remainDigits.FirstOrDefault(digit => digit > number[i]);
                 if (minimumDigit == 0)
                     continue;
 
                 remainDigits.Remove(minimumDigit);
-                var temp = number[..i] + minimumDigit + string.Join("", remainDigits);
-                return temp;
+                return number.Substring(0, i) + minimumDigit + string.Join("", remainDigits);
             }
 
             return "0";
