7747f87242f678bd63a895d279471719b648c83c
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:49:38 +0330

    Adds solutions to 9109

---
+++ Solutions/9109/c++/program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+const int maxn = 100;
+int counter[maxn];
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        counter[x - 1]++;
+    }
+
+    int mn = -1;
+    for (int i = 0; i < maxn; i++)
+        if (counter[i] > 0 && (mn == -1 || counter[mn] > counter[i]))
+            mn = i;
+
+    cout << mn + 1 << '\n';
+}
---
+++ Solutions/9109/java/program.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    final static int maxn = 100;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] counter = new int[maxn];
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            counter[x - 1]++;
+        }
+
+        int mn = -1;
+        for (int i = 0; i < maxn; i++)
+            if (counter[i] > 0 && (mn == -1 || counter[i] < counter[mn]))
+                mn = i;
+
+        System.out.println(mn + 1);
+    }
+}
