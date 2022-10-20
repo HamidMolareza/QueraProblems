d1450b69ce6896031c3544e28d97b342bd96489e
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:13:00 +0330

    Adds solutions to 140035

---
+++ Solutions/140035/c++/program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if (A % 2 == 0 || (B % 2 == 0 && C % 2 == 0))
+    {
+        cout << "good";
+    }
+    else
+    {
+        cout << "bad";
+    }
+    return 0;
+}
---
+++ Solutions/140035/java/program.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt() , b = scanner.nextInt() , c = scanner.nextInt();
+        if ((a%2 == 0) || ((b % 2 == 0) && (c % 2 == 0))){
+            System.out.print("good");}
+        else {
+            System.out.print("bad");
+        }
+        
+
+    }
+}
+
