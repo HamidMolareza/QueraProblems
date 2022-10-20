86733d49ad73c0c08647a67f73405be9fc4a879b
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:29:52 +0330

    Adds solutions to 15124

---
+++ Solutions/15124/c++/program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, x;
+    cin >> a >> b >> x;
+    int ans = 0;
+    for (int i = 1; i <= a; i++)
+        for (int j = 1; j <= b; j++)
+            if (a % i == 0 && b % j == 0 && i + j <= x)
+                ans++;
+    cout << ans << '\n';
+    return 0;
+}
---
+++ Solutions/15124/java/program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a = scanner.nextInt(), b = scanner.nextInt(), x = scanner.nextInt();
+        int ans = 0;
+        for (int i = 1; i <= a; i++)
+            for (int j = 1; j <= b; j++)
+                if (a % i == 0 && b % j == 0 && i + j <= x)
+                    ans++;
+        System.out.println(ans);
+    }
+}
