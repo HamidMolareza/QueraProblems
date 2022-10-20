3cb6f23b9cda9ad5b40525f824cb7139ff7f4b48
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 17:01:23 +0330

    Adds solutions to 3540

---
+++ Solutions/3540/c++/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, x, y;
+    cin >> n >> x >> y;
+    for (int i = 0; i * x <= n; i++)
+        if ((n - i * x) % y == 0)
+        {
+            cout << i << ' ' << (n - i * x) / y << '\n';
+            return 0;
+        }
+    cout << "-1\n";
+    return 0;
+}
---
+++ Solutions/3540/java/program.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), x = scanner.nextInt(), y = scanner.nextInt();
+        for(int i = 0; i <= n / x; i++)
+            if((n - i * x) % y == 0)
+            {
+                System.out.println(i + " " + (n - i * x) / y);
+                return ;
+            }
+        System.out.println(-1);
+    }
+
+}
