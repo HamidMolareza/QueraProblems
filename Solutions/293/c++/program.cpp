24575b2b06d1bb902dce7cef5ce86779a4c72355
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:31:10 +0330

    Adds solutions to 293

---
+++ Solutions/293/c++/program.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int is_prime(int num)
+{
+    if (num == 1)
+        return 0;
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        if (is_prime(i) == 1)
+            cout << i << endl;
+    }
+    return 0;
+}
---
+++ Solutions/293/java/program.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        for(int i = a; i <= b ;i ++)
+        {
+            if(is_prime(i) == 1)
+                System.out.println(i);
+        }
+    }
+
+    private static int is_prime(int num)
+    {
+        if(num == 1)
+            return 0;
+        for(int i = 2; i < num; i ++)
+        {
+            if(num % i == 0)
+                return 0;
+        }
+        return 1;
+    }
+}
