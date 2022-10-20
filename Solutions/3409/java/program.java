e285cd40b52c124ba4456b5227122f73fee80958
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:28:30 +0330

    Adds solutions to 3409

---
+++ Solutions/3409/java/program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+                System.out.print(i * j + " ");
+            System.out.println(" ");
+        }
+    }
+}
