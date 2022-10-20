c3705fa4c0aea1079e79821e2882ca136cfa8cd5
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 17:02:33 +0330

    Adds solutions to 10233

---
+++ Solutions/10233/c++/program.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int x_digits[10], current_digits[10];
+
+int main()
+{
+    int x;
+    cin >> x;
+    int counter = x;
+    while (x)
+    {
+        x_digits[x % 10]++;
+        x = x / 10;
+    }
+    while (counter <= 10 * 1000 * 1000)
+    {
+        counter++;
+        for (int i = 0; i < 10; i++)
+            current_digits[i] = 0;
+        int current = counter;
+        while (current)
+        {
+            current_digits[current % 10]++;
+            current = current / 10;
+        }
+        bool flag = false;
+        for (int i = 0; i < 10; i++)
+            if (current_digits[i] != x_digits[i])
+                flag = true;
+        if (!flag)
+            break;
+    }
+    if (counter > 10 * 1000 * 1000)
+        counter = 0;
+    cout << counter << '\n';
+    return 0;
+}
---
+++ Solutions/10233/java/program.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int x = scanner.nextInt();
+        int [] cntx = new int[10];
+        int extraX = x;
+        while(extraX > 0){
+            cntx[extraX % 10]++;
+            extraX /= 10;
+        }
+        for(int y = x + 1; y <= 1000 * 1000; y++){
+            int [] cnty = new int[10];
+            int extraY = y;
+            while(extraY > 0){
+                cnty[extraY % 10]++;
+                extraY /= 10;
+            }
+            boolean equal = true;
+            for(int i = 0; i <= 9; i++)
+                if(cntx[i] != cnty[i]){
+                    equal = false;
+                    break;
+                }
+            if(equal){
+                System.out.println(y);
+                return ;
+            }
+        }
+        System.out.println(0);
+    }
+
+}
