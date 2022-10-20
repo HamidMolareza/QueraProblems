1a3d5fe77a4eca6649b87c73a24801d8a9bbb43a
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:11:51 +0330

    Adds solutions to 3414

---
+++ Solutions/3414/c++/program.cpp
@@ -0,0 +1,19 @@
+
+// In the name of God
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    if (x1 == x2)
+        cout << "Vertical\n";
+    else if (y1 == y2)
+        cout << "Horizontal\n";
+    else
+        cout << "Try again\n";
+    return 0;
+}
---
+++ Solutions/3414/java/program.java
@@ -0,0 +1,17 @@
+//package com.company;
+
+import java.util.Scanner;
+
+public class p2q2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x0 = scanner.nextInt(), y0 = scanner.nextInt(), x1 = scanner.nextInt(), y1 = scanner.nextInt();
+        if(x0 == x1)
+            System.out.println("Vertical");
+        else if(y0 == y1)
+            System.out.println("Horizontal");
+        else
+            System.out.println("Try again");
+    }
+}
