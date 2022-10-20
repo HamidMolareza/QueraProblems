171cad846032a8a973a414f40ab2057dc3a1a6ab
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:58:04 +0330

    Adds solutions to 9110

---
+++ Solutions/9110/c++/program.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+const int maxn = 100000;
+int output[maxn];
+
+int main()
+{
+    int n = 1;
+    output[0] = 1;
+
+    while (n < maxn)
+    {
+        for (int i = 0; i < n; i++)
+            if (i + n < maxn)
+                output[i + n] = 1 - output[i];
+
+        n *= 2;
+    }
+
+    int l, r;
+    cin >> l >> r;
+
+    for (int i = l - 1; i < r; i++)
+        cout << output[i];
+    cout << '\n';
+    return 0;
+}
---
+++ Solutions/9110/java/program.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    final static int maxn = 100000;
+    public static void main(String[] args) {
+        int[] output = new int[maxn];
+        int n = 1;
+        output[0] = 1;
+
+        while (n < maxn) {
+            for (int i = 0; i < n; i++)
+                if (i + n < maxn)
+                    output[i + n] = 1 - output[i];
+        
+            n *= 2;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int l = scanner.nextInt(), r = scanner.nextInt();
+        
+        for (int i = l - 1; i < r; i++)
+            System.out.print(output[i]);
+        System.out.println();
+    }
+}
