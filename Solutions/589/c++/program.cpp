e972dd14bd8bb08517bf5ffd64c98187026437f1
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:25:55 +0330

    Adds solutions to 589

---
+++ Solutions/589/c++/program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long fact = 1;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        fact *= i + 1;
+    cout << fact << "\n";
+    return 0;
+}
---
+++ Solutions/589/java/program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long fact = 1;
+        for (int i = 0; i< n; i++)
+            fact *= i+1;
+        System.out.println(fact);
+    }
+}
