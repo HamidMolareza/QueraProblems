c1f929d97c3f62980921207b2ff939cbe11dc18a
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:17:35 +0330

    Adds solutions to 3029

---
+++ Solutions/3029/c++/program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y, x1, y1;
+    cin >> x >> y >> x1 >> y1;
+    if (x < x1)
+        cout << "Right";
+    else
+        cout << "Left";
+    return 0;
+}
---
+++ Solutions/3029/java/program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt(),y=scanner.nextInt();
+        int x1=scanner.nextInt(),y1=scanner.nextInt();
+        if(x1>x) System.out.println("Right");
+        else System.out.println("Left");
+    }
+}
