eeb710e9b09d75b1f8d0311c1d3cfc33ccf4e89e
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:10:24 +0330

    Adds solutions to 2886

---
+++ Solutions/2886/c++/program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    printf("%02d:%02d\n", (12 - a) % 12, (60 - b) % 60);
+    return 0;
+}
---
+++ Solutions/2886/java/program.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner=new Scanner(System.in);
+        
+        int hour = scanner.nextInt(), minute = scanner.nextInt();
+        
+        hour = 12 - hour;
+        hour = hour % 12;
+        
+        minute = 60 - minute;
+        minute = minute % 60;
+        
+        System.out.printf("%02d:%02d", hour, minute);
+    }
+}
