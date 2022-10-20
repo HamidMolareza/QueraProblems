efa8eaf946e2b49646a9e1f9e490525c685e64d2
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:53:56 +0330

    Adds solutions to 2529

---
+++ Solutions/2529/c++/program.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, answer = 0;
+    string s;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> s;
+
+        int distinct_characters = s.length();
+        for (int j = 0; s[j] != 0; j++)
+        {
+            int duplicate = 0;
+            for (int k = 0; k < j; k++)
+                if (s[k] == s[j])
+                    duplicate = 1;
+
+            distinct_characters -= duplicate;
+        }
+
+        if (answer < distinct_characters)
+            answer = distinct_characters;
+    }
+
+    cout << answer << '\n';
+    return 0;
+}
---
+++ Solutions/2529/java/program.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            
+            String word = scanner.next();
+            char[] s = word.toCharArray();
+            int distinctCharacters = word.length();
+            
+            for (int j = 0; j < s.length; j++) {
+                
+                int duplicate = 0;
+                
+                for (int k = 0; k < j; k++)
+                    if (s[k] == s[j])
+                        duplicate = 1;
+            
+                distinctCharacters -= duplicate;
+            }
+            
+            if (answer < distinctCharacters)
+                answer = distinctCharacters;
+        }
+        
+        System.out.println(answer);
+    }
+}
