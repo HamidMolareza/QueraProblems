eeb2c806c22652c6742e211da81967dca705e7f6
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:20:21 +0330

    Adds solutions to 20256

---
+++ Solutions/20256/c++/program.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c1, c2, c3, c4, c5;
+    cin >> c1 >> c2 >> c3 >> c4 >> c5;
+    int R = 0, Y = 0, G = 0;
+    // This piece of code repeats 5 times!
+    // 1
+    if (c1 == 'R')
+        R = R + 1;
+    if (c1 == 'Y')
+        Y = Y + 1;
+    if (c1 == 'G')
+        G = G + 1;
+
+    // 2
+    if (c2 == 'R')
+        R = R + 1;
+    if (c2 == 'Y')
+        Y = Y + 1;
+    if (c2 == 'G')
+        G = G + 1;
+
+    // 3
+    if (c3 == 'R')
+        R = R + 1;
+    if (c3 == 'Y')
+        Y = Y + 1;
+    if (c3 == 'G')
+        G = G + 1;
+
+    // 4
+    if (c4 == 'R')
+        R = R + 1;
+    if (c4 == 'Y')
+        Y = Y + 1;
+    if (c4 == 'G')
+        G = G + 1;
+
+    // 5
+    if (c5 == 'R')
+        R = R + 1;
+    if (c5 == 'Y')
+        Y = Y + 1;
+    if (c5 == 'G')
+        G = G + 1;
+
+    if (R >= 3 || (Y >= 2 && R >= 2) || G == 0)
+        cout << "nakhor lite\n";
+    else
+        cout << "rahat baash\n";
+    return 0;
+}
---
+++ Solutions/20256/java/program.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String label = s.next();
+        int R = 0, G = 0, Y = 0;
+        
+        if(label.charAt(0) == 'R') R++;
+        else if(label.charAt(0) == 'G') G++;
+        else Y++;
+        
+        if(label.charAt(1) == 'R') R++;
+        else if(label.charAt(1) == 'G') G++;
+        else Y++;
+        
+        if(label.charAt(2) == 'R') R++;
+        else if(label.charAt(2) == 'G') G++;
+        else Y++;
+        
+        if(label.charAt(3) == 'R') R++;
+        else if(label.charAt(3) == 'G') G++;
+        else Y++;
+        
+        if(label.charAt(4) == 'R') R++;
+        else if(label.charAt(4) == 'G') G++;
+        else Y++;
+        
+        if( (R>=3) || (R==2 && Y>=2) || (G==0) ) System.out.println("nakhor lite");
+        else System.out.println("rahat baash");
+    }
+}
