878db8732cbf44cb8d48d4ba1238d1e668309c3c
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:02:26 +0330

    Adds solutions for 17244

---
+++ Solutions/17244/c++/program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+    cout << k * (k + 1) / 2 << '\n';
+    return 0;
+}
---
+++ Solutions/17244/java/program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int x = k * (k + 1) / 2;
+        System.out.println(x);
+    }
+}
