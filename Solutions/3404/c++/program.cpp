981c0251d5756e1d2248005042c5752d982b7198
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:36:17 +0330

    Adds solutions to 3404

---
+++ Solutions/3404/c++/program.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double weight, height, bmi;
+    cin >> weight >> height;
+    bmi = weight / (height * height);
+    cout << setprecision(2) << fixed << bmi << "\n";
+
+    if (bmi < 18.5)
+    {
+        cout << "Underweight" << endl;
+    }
+    else if (bmi > 18.5 && bmi < 25)
+    {
+        cout << "Normal" << endl;
+    }
+    else if (bmi > 25 && bmi < 30)
+    {
+        cout << "Overweight" << endl;
+    }
+    else
+    {
+        cout << "Obese" << endl;
+    }
+    return 0;
+}
---
+++ Solutions/3404/java/program.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int weight = scanner.nextInt();
+        double height = scanner.nextDouble();
+
+        double bmi = weight/(height * height);
+        System.out.format("%.2f", bmi);
+        System.out.println();
+        if(bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if(bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normal");
+        } else if(bmi >= 25 && bmi < 30) {
+            System.out.println("Overweight");
+        } else if(bmi >= 30) {
+            System.out.println("Obese");
+        }
+    }
+}
