bb0d049732a266b9084928905866b713fb5eb96c
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:40:35 +0330

    Adds solutions to 20257

---
+++ Solutions/20257/c++/program.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int k, a, b, m, ans = 0;
+    cin >> k >> a >> b;
+    if (a > b)
+        swap(a, b);
+    m = b - a;
+    if (abs(a % k) < k - abs(a % k))
+    {
+        ans += abs(a % k);
+        if (a > 0)
+            a -= abs(a % k);
+        else
+            a += abs(a % k);
+    }
+    else
+    {
+        ans += k - abs(a % k);
+        if (a > 0)
+            a += k - abs(a % k);
+        else
+            a -= k - abs(a % k);
+    }
+    if (abs(b % k) <= k - abs(b % k))
+    {
+        ans += abs(b % k);
+        if (b > 0)
+            b -= abs(b % k);
+        else
+            b += abs(b % k);
+    }
+    else
+    {
+        ans += k - abs(b % k);
+        if (b > 0)
+            b += k - abs(b % k);
+        else
+            b -= k - abs(b % k);
+    }
+    ans += (b - a) / k;
+    ans = min(ans, m);
+    cout << ans << endl;
+    return 0;
+}
---
+++ Solutions/20257/java/program.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        long k = s.nextInt(), a = s.nextInt(), b = s.nextInt();
+        long a0 = a, b0 = b;
+        long t = 0;
+        while (a < 0) {
+            a += k;
+            b += k;
+        }
+        long mem = a % k;
+        if (mem > k/2) {
+            a += k - mem;
+            t += k - mem;
+        } else {
+            a -= mem;
+            t += mem;
+        }
+        mem = b % k;
+        if (mem > k/2) {
+            b += k - mem;
+            t += k - mem;
+        } else {
+            b -= mem;
+            t += mem;
+        }
+        t += (b - a) / k;
+        if(t > b0 - a0) {
+            System.out.println(b0 - a0);
+            return;
+        }
+        System.out.println(t);
+    }
+}
