e189a497f5d893262cd284be20798a060e8004a4
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:51:28 +0330

    Adds solutions to 6082

---
+++ Solutions/6082/c++/program.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    char a[n][m];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            if (a[i][j] == '*')
+                ans++;
+
+    cout << ans << '\n';
+    return 0;
+}
---
+++ Solutions/6082/java/program.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), m = scanner.nextInt();
+        
+        char[][] a = new char[n][m];
+        for (int i = 0; i < n; i++)
+            a[i] = scanner.next().toCharArray();
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++)
+                if (a[i][j] == '*')
+                    ans++;
+
+        System.out.println(ans);
+    }
+}
