058ad858649c7cc59dc9797c5adf3525d22090b8
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:24:50 +0330

    Adds solutions to 3537

---
+++ Solutions/3537/c++/program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "W";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "o";
+    }
+    cout << "w!";
+    return 0;
+}
---
+++ Solutions/3537/java/program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print("W");
+        for (int i = 0; i < n; i++)
+            System.out.print("o");
+        System.out.println("w!");
+    }
+}
