da7bd93aafbc62afdc4a8c5ab991994a27fcd8cd
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:07:19 +0330

    Adds solutions to 2637

---
+++ Solutions/2637/c++/program.cpp
@@ -0,0 +1,14 @@
+
+// In the name of God
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    // ceil(a/b) is equal to floor((a+b-1)/b)
+    cout << (n / 2 + 1) * ((n + 1) / 2 + 1) << '\n';
+    return 0;
+}
---
+++ Solutions/2637/java/program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println((n/2+1)*((n+1)/2+1));
+        /*
+        age mikhaim saghfe x/y ke x va y adad haie tabiei hstn ro hsb konim,
+        mitonim kharej ghesmate taghsime (x+y-1)/y ro dr nzr bgirim
+        */
+    }
+}
