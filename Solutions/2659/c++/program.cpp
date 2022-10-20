09efe49e1bb9550d549c61ba023a452868040d8b
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:42:45 +0330

    Adds solutions to 2659

---
+++ Solutions/2659/c++/program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    char s[n];
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+
+    char t[n];
+    for (int i = 0; i < n; i++)
+        cin >> t[i];
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        if (s[i] != t[i])
+            ans++;
+
+    cout << ans << '\n';
+    return 0;
+}
---
+++ Solutions/2659/java/program.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+		char[] s = scanner.next().toCharArray();
+		char[] t = scanner.next().toCharArray();
+
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+			if (s[i] != t[i]) 
+				ans++;
+
+		System.out.println(ans);
+    }
+}
