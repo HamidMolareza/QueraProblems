f55a6eed3bc0125958b918f4e77f083ebc8af721
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:38:21 +0330

    Adds solutions to 8901

---
+++ Solutions/8901/c++/program.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    char x, a, b, ans;
+    cin >> n >> x;
+    ans = x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+        if ((ans == a) || (ans == b))
+        {
+            if (ans != a)
+                ans = a;
+            else
+                ans = b;
+        }
+    }
+    cout << ans;
+    return 0;
+}
---
+++ Solutions/8901/java/program.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        char x = sc.next().charAt(0);
+
+        char ans = x;
+
+        for (int i = 0; i < n; i ++){
+            char a = sc.next().charAt(0);
+            char b = sc.next().charAt(0);
+
+            if( a == ans )
+                ans = b;
+            else if ( b == ans )
+                ans = a;
+        }
+        System.out.println(ans);
+    }
+}
