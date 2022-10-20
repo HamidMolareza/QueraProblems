c24a57331f6f15ea00e23b21322ad91397e8294e
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:05:51 +0330

    Adds solution to 2636

---
+++ Solutions/2636/c++/program.cpp
@@ -0,0 +1,23 @@
+
+// In the name of God
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Set up standard number of chess pieces
+    int standard_king = 1, standard_queen = 1, standard_rook = 2;
+    int standard_bishop = 2, standard_knight = 2, standard_soldier = 8;
+
+    // Get the number of chess pieces from input
+    int my_king, my_queen, my_rook, my_bishop, my_knight, my_soldier;
+    cin >> my_king >> my_queen >> my_rook >> my_bishop >> my_knight >> my_soldier;
+
+    // Output the differences
+    cout << standard_king - my_king << " " << standard_queen - my_queen << " ";
+    cout << standard_rook - my_rook << " " << standard_bishop - my_bishop << " ";
+    cout << standard_knight - my_knight << " " << standard_soldier - my_soldier << "\n";
+
+    return 0;
+}
---
+++ Solutions/2636/java/program.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int kingRequired   = 1;
+        int queenRequired  = 1;
+        int rookRequired   = 2;
+        int bishopRequired = 2;
+        int knightRequired = 2;
+        int pawnRequired   = 8;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int king   = scanner.nextInt();
+        int queen  = scanner.nextInt();
+        int rook   = scanner.nextInt();
+        int bishop = scanner.nextInt();
+        int knight = scanner.nextInt();
+        int pawn   = scanner.nextInt();
+        
+        System.out.println((kingRequired - king)      + " " + 
+                            (queenRequired - queen)   + " " + 
+                            (rookRequired - rook)     + " " +
+                            (bishopRequired - bishop) + " " + 
+                            (knightRequired - knight) + " " + 
+                            (pawnRequired - pawn));
+    }
+}
