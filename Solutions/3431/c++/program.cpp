0e5f7840741a4ae2b5c42249e5e3d7fae1bb9881
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 17:00:16 +0330

    Adds solutions to 3431

---
+++ Solutions/3431/c++/program.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string arr[55], s;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> s;
+    int len = s.size();
+    int answer = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+        {
+            bool flag = false;
+            for (int k = 0; s[k] && arr[i][j + k]; k++)
+                if (s[k] != arr[i][j + k])
+                    flag = true;
+            if (j + len > m)
+                flag = true;
+            answer = answer + !flag;
+
+            flag = false;
+            for (int k = 0; s[k] && arr[i + k][j]; k++)
+                if (s[k] != arr[i + k][j])
+                    flag = true;
+            if (i + len > n)
+                flag = true;
+            answer = answer + !flag;
+        }
+    cout << answer << '\n';
+    return 0;
+}
---
+++ Solutions/3431/java/program.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n = scanner.nextInt(),m=scanner.nextInt();
+        String [] a = new String[51];
+        
+        for(int i = 0; i < n; i++)
+            a[i]=scanner.next();
+        
+        String pattern = scanner.next();
+
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j + pattern.length() <= m; j++)
+            {
+                boolean equal = true;
+                for(int k = 0; k < pattern.length(); k++)
+                    if(pattern.charAt(k) != a[i].charAt(j + k))
+                    {
+                        equal=false;
+                        break;
+                    }
+                if(equal) ans++;
+            }
+        for(int i = 0; i + pattern.length() <= n; i++)
+            for(int j = 0; j < m; j++)
+            {
+                boolean equal = true;
+                for(int k = 0; k < pattern.length(); k++)
+                    if(pattern.charAt(k) != a[i + k].charAt(j))
+                    {
+                        equal = false;
+                        break;
+                    }
+                if(equal) ans++;
+            }
+        System.out.println(ans);
+    }
+
+}
