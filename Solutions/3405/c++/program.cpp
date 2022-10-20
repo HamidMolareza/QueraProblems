68f24c02cbe0bba4ffb274b0fedb24a644f5be8c
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:44:02 +0330

    Adds solutions to 3405

---
+++ Solutions/3405/c++/program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    int arr[1000];
+    int i = 0;
+    while (num != 0)
+    {
+        arr[i] = num;
+        cin >> num;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--)
+    {
+        cout << arr[j] << endl;
+    }
+    return 0;
+}
---
+++ Solutions/3405/java/program.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[1000];
+        
+        int num = scanner.nextInt();
+        int i = 0;
+        
+        while(num != 0) {
+            arr[i] = num;
+            num = scanner.nextInt();
+            i ++;
+        }
+
+        for (int j = i-1; j >= 0; j--)
+            System.out.println(arr[j]);        
+    }
+}
