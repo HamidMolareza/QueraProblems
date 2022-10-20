9535ca109ba7f1c21565b57341b0b250c9b21a39
Hamid Molareza <kitm.hamid@gmail.com>
2022-10-20 16:15:24 +0330

    Adds solutions to 3406

---
+++ Solutions/3406/c++/program.cpp
@@ -0,0 +1,26 @@
+
+// In the name of God
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << a << " = " << b << '\n';
+    }
+    else
+    {
+        int reverse_a = (a % 10) * 100 + (a / 10 % 10) * 10 + (a / 100);
+        int reverse_b = (b % 10) * 100 + (b / 10 % 10) * 10 + (b / 100);
+        if (reverse_a < reverse_b)
+            cout << a << " < " << b << '\n';
+        else
+            cout << b << " < " << a << '\n';
+    }
+    return 0;
+}
---
+++ Solutions/3406/java/program.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt(),b=scanner.nextInt();
+        int newA=(a/100)+((a/10)%10)*10+(a%10)*100;
+        int newB=(b/100)+((b/10)%10)*10+(b%10)*100;
+        if(newA==newB)
+        {
+            System.out.println(a+" = "+b);
+        }
+        else if(newA<newB)
+        {
+            System.out.println(a+" < "+b);
+        }
+        else
+        {
+            System.out.println(b+" < "+a);
+        }
+    }
+}
