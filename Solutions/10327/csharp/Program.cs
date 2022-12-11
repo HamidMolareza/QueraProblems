c877eb64a6ea47bffb2fc567abc1cebcd4a6e35e
Hamid Molareza <kitm.hamid@gmail.com>
2022-12-11 19:57:07 +0330

    Solves 10327 with csharp

---
+++ Solutions/10327/csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            var firstLine = Console.ReadLine().Split(' ');
+            var numOfCodes = Convert.ToInt32(firstLine[0]);
+            var validChars = firstLine[1].Distinct().ToList();
+
+            for (var i = 0; i < numOfCodes; i++) {
+                var codeChars = Console.ReadLine().Distinct().ToList();
+                var isValid = IsEqual(codeChars, validChars);
+                Console.WriteLine(isValid ? "Yes" : "No");
+            }
+        }
+
+        private static bool IsEqual<T>(IReadOnlyCollection<T> a, IReadOnlyCollection<T> b) =>
+            !a.Except(b).Any() && !b.Except(a).Any();
+    }
+}
