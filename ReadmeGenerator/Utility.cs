defc009587935375099ab41e1b376dcd640fc169
Hamid Molareza <kitm.hamid@gmail.com>
2022-12-18 21:57:01 +0330

    Minor updates

--- ReadmeGenerator/Utility.cs
+++ ReadmeGenerator/Utility.cs
@@ -3,7 +3,7 @@ using System;
 namespace Quera {
     public static class Utility {
         public static TResult ConvertTo<TSource, TResult>(this TSource source) =>
-            (TResult) Convert.ChangeType(source, typeof(TResult)) ??
+            (TResult) Convert.ChangeType(source, typeof(TResult))! ??
             throw new Exception($"Can not convert to {typeof(TResult)}");
     }
 }
