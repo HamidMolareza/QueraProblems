ed44a92e63c2919c6f45bf177078abd5781fea61
Hamid Molareza <kitm.hamid@gmail.com>
2022-12-30 04:58:31 +0330

    Solves 140035 with csharp

---
+++ Solutions/140035/csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            if (IsOdd(number) && IsPrime(number)) {
+                Console.WriteLine("zoj");
+            }
+            else {
+                Console.WriteLine("fard");
+            }
+        }
+
+        private static bool IsPrime(int number) {
+            if (number < 2) return false;
+            if (number < 4) return true;
+
+            for (var i = 2; i < Math.Sqrt(number) + 1; i++) {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsOdd(int number) => number % 2 != 0;
+    }
+}
