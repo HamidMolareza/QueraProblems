83960cbabd9e1bd76264aa8c49562a763d1d7258
Hamid Molareza <kitm.hamid@gmail.com>
2023-01-01 18:51:05 +0330

    Solves 8838 with csharp

---
+++ Solutions/8838/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            var inputs = Console.ReadLine().Split(' ');
+
+            var repeat = Convert.ToInt32(inputs[0]);
+            for (var i = 0; i < repeat; i++)
+                Console.Write("copy of ");
+            Console.WriteLine(inputs[1]);
+        }
+    }
+}
