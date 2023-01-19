bd26aa42868ff36aa4e732657c2f987af9704d89
Hamid Molareza <kitm.hamid@gmail.com>
2023-01-19 16:05:21 +0330

    feat(Templates): add samples for readline to javascript template

--- Templates/javascript/program.js
+++ Templates/javascript/program.js
@@ -11,6 +11,11 @@ let solution = new Solution();
 if (typeof readline === 'function') {
     // This is for Quera judge
     solution.solve(readline());
+
+    /* Samples:
+    const [x1, y1, x2, y2] = readline().split(" ").map(n => +n);
+    const A = +readline();
+     */
 } else {
     //This is for manual test
     solution.solve("Hamid");
